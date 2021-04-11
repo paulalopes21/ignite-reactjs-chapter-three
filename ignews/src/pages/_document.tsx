f6939ce6322471dcfa7def8fe9f03483ce5d63f6
Paula Lopes <pls21@msn.com>
2021-04-10 22:25:48 -0300

    Title dinâmico por página

--- ignews/src/pages/_document.tsx
+++ ignews/src/pages/_document.tsx
@@ -8,7 +8,7 @@ export default class MyDocument extends Document {
             <link rel="preconnect" href="https://fonts.gstatic.com" />
             <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,900;1,700&display=swap" rel="stylesheet" />
 
-            <title>ig.news</title>
+            
           </Head>
           <body>
             <Main />
