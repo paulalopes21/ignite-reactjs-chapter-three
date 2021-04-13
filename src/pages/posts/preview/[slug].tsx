e7b97f9f2730377cb9f536ea612940e2fb0a165d
Paula Lopes <pls21@msn.com>
2021-04-13 14:38:02 -0300

    Validando assinatura ativava

--- src/pages/posts/preview/[slug].tsx
+++ src/pages/posts/preview/[slug].tsx
@@ -1,4 +1,4 @@
-import { GetStaticProps } from 'next';
+import { GetStaticPaths, GetStaticProps } from 'next';
 import { useSession } from 'next-auth/client';
 import Head from 'next/head';
 import Link from 'next/link';
@@ -55,7 +55,7 @@ export default function PostPreview ({ post }: PostPreviewProps) {
   )
 }
 
-export const getStaticPaths = () => {
+export const getStaticPaths: GetStaticPaths = async () => {
   return {
     paths: [],
     fallback: 'blocking'
@@ -82,7 +82,8 @@ export const getStaticProps: GetStaticProps = async ( { params }) => {
   }
 
   return {
-    props: { post }
+    props: { post },
+    redirect: 60 * 30 // 30 minutes
   }
 
 }
